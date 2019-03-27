43db2961215095703513dfadc47f7de5ed43afc3
Freddy Drennan <drennanfreddy@gmail.com>
2019-03-27 10:39:50 -0600

    update

--- misc/personal.py
+++ misc/personal.py
@@ -23,7 +23,7 @@ ctx = Context("personal")
 
 keymap = {
     "perpipe": [Key("cmd-shift-m"), Key("enter")],
-    "our chunk": {Key("```{r}"), Key("enter"), Key("`")}
+    "our chunk": {Key("`"), Key("enter"), Key("`")}
 }
 
 ctx.keymap(keymap)
