90485b19df4979b867e9483c5742ecce7047f0d5
Freddy Drennan <drennanfreddy@gmail.com>
2019-04-13 14:53:38 -0600

    update

--- misc/personal.py
+++ misc/personal.py
@@ -25,8 +25,7 @@ keymap = {
     "perpipe": [Key("cmd-shift-m"), Key("enter")],
     "our chunk": ["```{r, message=FALSE, warning=FALSE}", Key("enter"), Key("enter"), "```", Key("up")],
     "pie chunk": ["```{python}", Key("enter"), Key("enter"), "```", Key("up")],
-    "our pat": [Key("space"), "<-", Key("enter")],
-    "pie chunk": ["```{python}", Key("enter"), Key("enter"), "`", Key("up")]
+    "our pat": [Key("space"), "<-", Key("enter")]
 }
 
 ctx.keymap(keymap)
