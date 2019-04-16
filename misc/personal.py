7bdb8796e12a760d830a7921f3fe3aa769590e4a
Freddy Drennan <drennanfreddy@gmail.com>
2019-04-16 13:21:32 -0600

    update

--- misc/personal.py
+++ misc/personal.py
@@ -23,7 +23,7 @@ ctx = Context("personal")
 # }
 
 keymap = {
-    "ack gro": (True, lambda i, word, _: word[0:1].upper()),
+    "acronym": (True, lambda i, word, _: word[0:1].upper()),
     "our pipe": [Key("cmd-shift-m"), Key("enter")],
     "our chunk": ["```{r, message=FALSE, warning=FALSE}", Key("enter"), Key("enter"), "```", Key("up")],
     "pie chunk": ["```{python}", Key("enter"), Key("enter"), "```", Key("up")],
