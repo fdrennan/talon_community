b0e084582979a61ec7f8d68cb4f5b850b6f03f05
Freddy Drennan <drennanfreddy@gmail.com>
2019-04-15 19:20:52 -0600

    Update

---
+++ misc/R.py
@@ -0,0 +1,32 @@
+from talon.voice import Context, Key
+
+ctx = Context("personal")
+#  this is a mapping for scrolling and other mouse utilities
+#
+# keymap = {
+
+#     # scrolling
+#     "scroll down": [Key("down")] * 30,
+#     "scroll up": [Key("up")] * 30,
+#     # NB home and end do not work in all apps
+#     "(scroll way down | doomway)": Key("cmd-down"),
+#     "(scroll way up | jeepway)": Key("cmd-up"),
+#     "page up": [Key("pageup")],
+#     "page down": [Key("pagedown")],
+#     # searching
+#     "(search | marco)": Key("cmd-f"),
+#     "marneck": Key("cmd-g"),
+#     "marpreev": Key("cmd-shift-g"),
+#     "marthis": [Key("alt-right"), Key("shift-alt-left"), Key("cmd-f"), Key("enter")],
+# }
+
+
+keymap = {
+    "perpipe": [Key("cmd-shift-m"), Key("enter")],
+    "our chunk": ["```{r, message=FALSE, warning=FALSE}", Key("enter"), Key("enter"), "```", Key("up")],
+    "pie chunk": ["```{python}", Key("enter"), Key("enter"), "```", Key("up")],
+    "our pat": [Key("space"), "<-", Key("enter")],
+    "our calm some": ["sum()", Key("left")]
+}
+
+ctx.keymap(keymap)
