ee7de6fdfc4adef34496eb4d627253cd272f781b
Freddy Drennan <drennanfreddy@gmail.com>
2019-04-17 11:32:32 -0600

    update

--- misc/python.py
+++ misc/python.py
@@ -21,8 +21,12 @@ ctx = Context("python")
 # }
 
 keymap = {
-    "none pie": ["np"],
-    "import none pie": ["import numpy as np"]
+    "import none pie": ["import numpy as np"],
+    "none pie": ["np."],
+    "import pandas": ["import pandas as pd"],
+    "pandas": ["pd."],
+    "import mat plot": ["import matplotlib.pyplot as plt"],
+    "mat plot": ["plt."],
 }
 
 ctx.keymap(keymap)
