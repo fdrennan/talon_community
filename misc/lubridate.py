7c04fbf91cfb992b03aa73474b41addbfffd841d
Freddy Drennan <drennanfreddy@gmail.com>
2019-04-21 12:43:02 -0600

    update

--- misc/lubridate.py
+++ misc/lubridate.py
@@ -25,8 +25,7 @@ keymap = {
     "import blooper": ["library(lubridate)"],
     "blooper package": ["lubridate::"],
     "blooper as date time": ["as_datetime()", Key("left")],
-    "blooper why them dee": ["ymd()", Key("left")],
-    "blooper why them dee three": ["ymd_hms()", Key("left")],
+    "blooper why them dee": ["ymd", Key("tab")]
 
 }
 
