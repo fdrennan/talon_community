52e48c4f42dc89401756462afe820588bdfef5c5
Freddy Drennan <drennanfreddy@gmail.com>
2019-04-27 09:09:26 -0600

    cd

--- misc/personal.py
+++ misc/personal.py
@@ -22,6 +22,7 @@ ctx = Context("personal")
 
 keymap = {
     "jeera": ["JIRA"],
+    "dren are": ["drenr.com"],
     "sequel": ["SQL"],
     "jet pole": ["git pull "],
     "our double down": [Key("down"), Key("down")],
