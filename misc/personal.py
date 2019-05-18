d3c239eb556098d1e33554701e65ac8e3994245a
Freddy Drennan <drennanfreddy@gmail.com>
2019-05-18 06:44:01 -0600

    Update stuff

--- misc/personal.py
+++ misc/personal.py
@@ -21,6 +21,16 @@ ctx = Context("personal")
 # }
 
 keymap = {
+    "import none pie": ["import numpy as np"],
+    "none pie": ["np."],
+    "import pandas": ["import pandas as pd"],
+    "pandas": ["pd."],
+    "import mat plot": ["import matplotlib.pyplot as plt"],
+    "mat plot": ["plt."],
+    "pandas call names": [".columns"],
+    "pandas group by": [".groupby(['Animals'])"],
+    "pandas summarise": [".agg({'column': 'function'})"],
+    "pandas apply": [".agg({'column': 'function'})"],
     "ellis": ["ls -la"],
     "post grass": ["postgres"],
     "big our": ["biggr"],
