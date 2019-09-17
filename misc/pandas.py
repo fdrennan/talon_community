6378e5a7a20158f5ac6a3f6974bd9f87fe8d6545
Freddy Drennan <drennanfreddy@gmail.com>
2019-09-16 19:10:14 -0600

    update

---
+++ misc/pandas.py
@@ -0,0 +1,17 @@
+from talon.voice import Context, Key
+
+ctx = Context("pandas")
+
+
+keymap = {
+    "import pandas": ["import pandas as pd"],
+    "pandas": ["pd."],
+    "pandas call names": [".columns"],
+    "pandas group by": [".groupby(['Animals'])"],
+    "pandas summarise": [".agg({'column': 'function'})"],
+    "pandas apply": [".agg({'column': 'function'})"],
+    "pandas eye": [".iloc[]", Key("left")],
+
+}
+
+ctx.keymap(keymap)
