dfa2a3ee63c2daedfc6c106515c34decbb1e4fc3
Freddy Drennan <drennanfreddy@gmail.com>
2019-10-06 12:06:27 -0600

    update

--- misc/git_helpers.py
+++ misc/git_helpers.py
@@ -4,7 +4,9 @@ ctx = Context("githelpers")
 
 
 keymap = {
-    "jet lazy": ["git add --all && git commit -m 'update'"]
+    "jet lazy commit": ["git add --all && git commit -m 'update'"],
+    "jet lazy push": ["git push origin master"],
+    "jet lazy pull": ["git pull origin master"],
 }
 
 ctx.keymap(keymap)
